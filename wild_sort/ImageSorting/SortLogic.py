41b3fc544aec264587fc5d17771213736b1c3efe
CaelCoruscare <caelsumner@gmail.com>
2023-09-28 09:11:28 +0300

    Cleaned up some old code that used Enums and edge cases

--- wild_sort/ImageSorting/SortLogic.py
+++ wild_sort/ImageSorting/SortLogic.py
@@ -44,61 +44,6 @@ def previousPhoto():
 
     Screens.screenManager.back()
 
-def _back():
-    #Edge cases are handled in tryBack()
-
-    ui.flashIcon('back')
-    ui.set_Photo(DataManager.getPhotoURL(index.photo))
-
-    currentCategory = DataManager.getCategory(index.category)
-    ui.set_PhotoCounter(currentCategory.getPhotoCounter(index.photo))
-
-def _categoryBack():
-    if index.category == 0:
-        return
-    
-    global edgeCase
-    edgeCase = EdgeCase.SHOWING_NEXT_CATEGORY_WILL_BE
-
-    index.category -= 1
-    nextCategory = DataManager.getCategory(index.category + 1)
-    ui.set_NextCategoryWillBe(nextCategory.title)
-    ui.set_PhotoCounter('-/' + str(nextCategory.countPhotos()))
-    ui.set_Category(None)
-    ui.flashIcon('back')
-
-    index.photo = len(DataManager.photoURLs) - 1
-
-def _handleBackEdgeCases():
-    global edgeCase
-
-    if edgeCase == EdgeCase.SHOWING_FOLDER_AREA or edgeCase == EdgeCase.SHOWING_TUTORIAL_KEYS or edgeCase == EdgeCase.FIRST_SHOWING_NEXT_CATEGORY:
-        return True #Ignore user input before sorting starts
-
-    if edgeCase == EdgeCase.AT_END_OF_CATEGORY:
-        edgeCase = EdgeCase.NONE
-        tryBack()
-        return True
-
-    if edgeCase == EdgeCase.SHOWING_NEXT_CATEGORY_WILL_BE:
-        edgeCase = EdgeCase.AT_END_OF_CATEGORY
-        ui.set_NextCategoryWillBe(None)
-        ui.set_Category(DataManager.getCategory(index.category).title)
-
-        #This is to avoid hitting handleBackEdgeCases() again
-        if DataManager.checkForSkip(index):
-            tryBack()
-        else:
-            _back()
-
-        return True
-    
-    if index.photo == 0:
-        _categoryBack()
-        return True
-
-    return False
-
 def recordData(dataValue):
     """Sets data based on the index.photo"""
     category = DataManager.getCategory(index.category)
